963801960bb7dd44d460a33e9cf814d1b22bac04
Iolanta26 <iolanta.u@gmail.com>
2021-08-17 10:11:18 +0300

    style added

--- src/components/TotalCompleteItems.js
+++ src/components/TotalCompleteItems.js
@@ -1,11 +1,15 @@
 import React from "react";
 import { useSelector } from "react-redux";
 
+import "../general.css";
+
 const TotalCompleteItems = () => {
   const completedTodo = useSelector((state) =>
     state.todos.filter((todo) => todo.completed === true)
   );
-  return <h4 className="mt-3">Total Complete Items: {completedTodo.length}</h4>;
+  return (
+    <h4 className="complete">Total Complete Items: {completedTodo.length}</h4>
+  );
 };
 
 export default TotalCompleteItems;
